9e1a4a4496dcd87476de928e359f26e4ca963cd6
Matheus Castiglioni <mahenrique94@users.noreply.github.com>
2024-11-14 22:59:02 -0300

    Keyboard navigation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const tabs = document.querySelectorAll(".tabs_tab");
-const panels = document.querySelectorAll(".tabs_panel");
+const tabs = document.querySelectorAll('[role="tab"]');
+const panels = document.querySelectorAll('[role="tabpanel"]');
+const tabList = document.querySelectorAll('[role="tablist"]');
 
 if (tabs && tabs.length > 0) {
   tabs.forEach((tab) => {
@@ -14,6 +15,7 @@ if (tabs && tabs.length > 0) {
 
       if (activeTab) {
         activeTab.setAttribute("aria-selected", false);
+        activeTab.tabIndex = -1;
       }
 
       if (activePanel) {
@@ -25,12 +27,74 @@ if (tabs && tabs.length > 0) {
       }
 
       event.target.setAttribute("aria-selected", true);
+      event.target.tabIndex = 0;
+    });
+
+    tab.addEventListener("keydown", (event) => {
+      switch (event.key) {
+        case "Tab":
+          event.preventDefault();
+          const activePanel = document.querySelector(
+            ".tabs_panel:not(.is-hide)"
+          );
+          const tabbables = activePanel.querySelectorAll(
+            'a[href], button:not([disabled]), textarea, input[type="text"]:not([disabled]), ' +
+              'input[type="radio"]:not([disabled]), input[type="checkbox"]:not([disabled]), ' +
+              'select:not([disabled]), [tabindex]:not([tabindex="-1"])'
+          );
+
+          if (tabbables && tabbables.length > 0) {
+            tabbables[0].focus();
+            activePanel.tabIndex = -1;
+          } else {
+            activePanel.focus();
+          }
+
+          break;
+        case "ArrowLeft":
+          const leftIndex = Array.from(
+            event.target.parentNode.parentNode.children
+          ).indexOf(event.target.parentNode);
+
+          if (leftIndex > 0) {
+            event.target.parentNode.previousElementSibling.firstElementChild.focus();
+          } else {
+            event.target.parentNode.parentNode.lastElementChild.firstElementChild.focus();
+          }
+
+          break;
+        case "ArrowRight":
+          const rightIndex = Array.from(
+            event.target.parentNode.parentNode.children
+          ).indexOf(event.target.parentNode);
+
+          if (
+            rightIndex <
+            event.target.parentNode.parentNode.children.length - 1
+          ) {
+            event.target.parentNode.nextElementSibling.firstElementChild.focus();
+          } else {
+            event.target.parentNode.parentNode.firstElementChild.firstElementChild.focus();
+          }
+
+          break;
+        case "Home":
+          event.target.parentNode.parentNode.firstElementChild.firstElementChild.focus();
+          break;
+        case "End":
+          event.target.parentNode.parentNode.lastElementChild.firstElementChild.focus();
+          break;
+        default:
+          break;
+      }
     });
 
     tab.setAttribute("aria-selected", false);
+    tab.tabIndex = -1;
   });
 
   tabs[0].setAttribute("aria-selected", true);
+  tabs[0].tabIndex = 0;
 }
 
 if (panels && panels.length > 0) {
